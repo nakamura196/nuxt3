f51fe826eafb1b58e73276e279f3db393b2bec93
Satoru <you@example.com>
2022-10-11 23:07:02 +0900

    feat: gh test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    nitro: {
-        preset: "aws-lambda",
-        serveStatic: true
-      },
-})
+  nitro: {
+    //preset: "aws-lambda",
+    //serveStatic: true
+  },
+});
